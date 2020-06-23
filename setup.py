ad79ff5c0d939005f2a68991891a5b5c9854f4d9
klara.mall <klara.mall@kit.edu>
2020-06-23 13:21:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+
 from setuptools import setup
 
-setup()
+setup(
+    name = 'odr',
+    py_modules = ['netdb_webapi'],
+    version = '0.0.1',
+    description = 'OpenVPN DHCP requestor',
+    author='Klaras Armee',
+    author_email='pony@kit.fail',
+    url='https://git.scc.kit.edu/scc-net/odr',
+    classifiers = [
+        # Verhindert versehentliches hochladen auf öffentlichen Repos, z.B.
+        # PyPi
+        'Private :: Do not upload',
+    ],
+)
